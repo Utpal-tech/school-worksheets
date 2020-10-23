18cd5f3203cc651bebcff088d7729f36c3960678
Utpal Kumar <73326144+Utpal-tech@users.noreply.github.com>
2020-10-23 22:53:53 +0530

    small correction

--- WS-9[2].py
+++ WS-9[2].py
@@ -1,11 +1,9 @@
 #WAP to input a number and prints it square and cube.
-print("This program will take 2 inputs and calculate their squre and cube.")
+print("This program will take a inputs and calculate its squre and cube.")
 def calculate():
     try:
         num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number"))
-        print("Squre of",num1,"is",num1**2,"and its cubeis",num1**3,) 
-        print("Squre of",num2,"is",num2**2,"and its cubeis",num2**3,) 
+        print("Squre of",num1,"is",num1**2,"and its cubeis",num1**3,)
         conTinue()
     except:
         print("Please provide correct input")
